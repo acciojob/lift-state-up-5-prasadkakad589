5839427580dc0fc1663406561c383021274c2371
prasadkakad589 <prasadkakad118@gmail.com>
2026-06-28 10:17:36 +0530

    Solved lift state up problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,59 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
+const Login = ({ login }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <input 
+        type="text" 
+        placeholder="Username"
+      />
+
+      <br /><br/>
+
+      <input 
+        type="password" 
+        placeholder="Password"
+      />
+
+      <br /><br/>
+
+      <button type="submit">
+        Login
+      </button>
+
+    </form>
+  );
+};
+
+
 const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+
+      {
+        isLoggedIn ? <h2>You are logged in !</h2>:<Login login={login}/>
+      }
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
